6a68ca235b5252c62638ea1531b154044e655f45
Roman Vasilyev <sektor_wins@mail.ru>
2020-01-13 23:13:37 +0500

    Временное решение для корректного запуска приложения в режиме разработки с новой версией библиотеки multi-download.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -164,7 +164,12 @@ module.exports = function (grunt) {
     wiredep: {
       app: {
         src: ['<%= yeoman.app %>/index.html'],
-        ignorePath:  /\.\.\//
+        ignorePath:  /\.\.\//,
+        overrides: {
+          'multi-download': {
+            'main': 'browser.js'
+          }
+        }
       },
       sass: {
         src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
